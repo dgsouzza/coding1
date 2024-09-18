755c47eccafa23464f649fe558e4b3f9f446ddf0
dgsouzza <douguiie.souzza17@gmail.com>
2024-09-18 18:05:03 +0000

    criamos algoritmo boletim

---
+++ aula.js
@@ -0,0 +1,43 @@
+
+/*var rotulo = "Natalya"
+rotulo = 12345;
+console.log(rotulo);
+*/
+
+
+/*console.log('hello' + '' + 'world')
+*/
+
+myName="Maria";
+/*console.log('hello'+' '+myName)
+*/
+
+/*myName2='2'
+console.log('Hello $ {myName2}')
+*/
+
+/*const numero1 = 10;
+const numero2 = 70;
+const soma = numero1 + numero2;
+console.log(soma);
+*/
+
+/*const numero1 = 50;
+const numero2 = 5
+const divisão = numero1 / numero2
+console.log(divisão)*/
+
+/*const numero1 = 35;
+const numero2 = 7;
+const menos = numero1 - numero2
+console.log(menos)
+*/
+
+/*const numero1 = 15;
+const numero2 = 5;
+const multiplicacao = numero1 * numero2
+console.log('o resultado e:' + multiplicacao)/*
+
+/*const media = (nota1 + nota2 + nota3)/3
+concole.log('O resultado  da media e... ${media}`)*/
+
